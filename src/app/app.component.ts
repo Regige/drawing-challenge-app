d8727f333e2c045b807a596bbb9c22925d6d7301
Regige <144334703+Regige@users.noreply.github.com>
2025-06-28 16:14:17 +0200

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink } from '@angular/router'; // Import RouterLink
+import { FooterComponent } from "./components/shared/footer/footer.component";
+import { HeaderComponent } from "./components/shared/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink], // Add RouterLink here
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
